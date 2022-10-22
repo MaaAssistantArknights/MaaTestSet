278cb48e97e8ece446c1b1b49487a9ef5fa14b14
MistEO <mistereo@hotmail.com>
2022-10-22 17:22:36 +0800

    chore: refactor file struct, add ja_jp drops
    ja_jp from https://github.com/MaaAssistantArknights/MaaAssistantArknights/issues/2363

--- de-duplication.py
+++ de-duplication.py
@@ -8,15 +8,18 @@ import os
 
 def de(working_dir):
     md5_set = set()
-    for file_name in os.listdir(working_dir):
-        with open(working_dir + file_name, 'rb') as fp:
-            data = fp.read()
-        file_md5 = hashlib.md5(data).hexdigest()
-        if file_md5 in md5_set:
-            print('Duplicate found: {}'.format(file_name))
-            os.remove(working_dir + file_name)
-        else:
-            md5_set.add(file_md5)
+    for client_name in os.listdir(working_dir):
+        client_dir = working_dir + client_name + '/'
+        for file_name in os.listdir(client_dir):
+            filepath = client_dir + file_name
+            with open(filepath, 'rb') as fp:
+                data = fp.read()
+            file_md5 = hashlib.md5(data).hexdigest()
+            if file_md5 in md5_set:
+                print('Duplicate found: {}'.format(filepath))
+                os.remove(filepath)
+            else:
+                md5_set.add(file_md5)
 
 
 de('./drops/screenshots/')
